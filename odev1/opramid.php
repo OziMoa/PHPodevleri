b8f7dd4c06f6e9a8d2d4be516875338708752d90
Mert Ozan <mert.ozan.aydogan@hotmail.com>
2023-08-09 23:14:36 +0300

    odev1 tamamlandı

---
+++ odev1/opramid.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+function triangle($o){
+    $i = 0;
+    while($i < $o){
+        for ($j = 0; $j < $i; $j++){
+            echo "o ";
+        }
+        echo "<br>";
+        $i++;
+    }
+
+}
+
+triangle(15);
+
+
+?>
